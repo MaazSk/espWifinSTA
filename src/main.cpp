ecc7a8febe134dd2bb06de9c4805107e1a7da9e3
MaazSk <maazshaikh05@gmail.com>
2023-10-01 13:25:44 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+
+// Set WiFi credentials
+#define WIFI_SSID "Sophia"
+#define WIFI_PASS "3@5y2cr@ck"
+
+// Set AP credentials
+#define AP_SSID "FreeWiFi"
+#define AP_PASS ""
+
+void setup()
+{
+  // Setup serial port
+  Serial.begin(115200);
+  Serial.println();
+
+  // Begin Access Point
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(AP_SSID);
+
+  // Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  // Connecting to WiFi...
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+
+  // Connected to WiFi
+  Serial.println();
+  Serial.println("Connected!");
+  Serial.print("IP address for network ");
+  Serial.print(WIFI_SSID);
+  Serial.print(" : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("IP address for network ");
+  Serial.print(AP_SSID);
+  Serial.print(" : ");
+  Serial.print(WiFi.softAPIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+}
